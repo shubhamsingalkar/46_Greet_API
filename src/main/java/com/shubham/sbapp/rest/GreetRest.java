e9ba8cceade52d06fe4d5db1623abd73e14f2e4c
Shubham V Singalkar <ssingalkar@avaya.com>
2025-07-03 14:28:12 +0530

    Adding Environment interface to print the port number...

--- src/main/java/com/shubham/sbapp/rest/GreetRest.java
+++ src/main/java/com/shubham/sbapp/rest/GreetRest.java
@@ -1,6 +1,7 @@
 package com.shubham.sbapp.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,12 +13,17 @@ public class GreetRest {
 	@Autowired
 	private  WelcomeApiCall welcomeApiCall;
 	
+	@Autowired
+	private Environment env;
+	
 	@GetMapping("/greet")
 	public String getGreetmsg(){
 		
+		String port = env.getProperty("server.port");
+		
 		String welcomeApi = welcomeApiCall.invokeWelcomeApi();
 		
-		String greet = "Good morning...";
+		String greet = "Good morning...Greet port ("+port+")";
 		
 		return greet+" "+welcomeApi;
 	}
